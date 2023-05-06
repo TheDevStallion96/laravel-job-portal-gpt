679c038eb2010f3dd56381c8b8a2c1d0be5ce54f
Tristan Elliott <declanjamesdevilliers2722@gmail.com>
2023-05-06 17:21:22 +0200

    JobController Updated

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Job;
 use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
-use Illuminate\Http\Client\Request;
-use Illuminate\Http\Request as HttpRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class JobController extends Controller
 {
@@ -30,15 +30,24 @@ class JobController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(HttpRequest $request)
+    public function store(Request $request)
     {
+        $this->validateWith([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'salary' => 'required',
+            'location' => 'required',
+        ]);
+
         $job = new Job();
         $job->title = $request->title;
         $job->description = $request->description;
-        $job->location = $request->location;
         $job->salary = $request->salary;
+        $job->location = $request->location;
         $job->save();
-        return redirect()->route('jobs.index')->with('success', 'Job created successfully.');
+
+        return redirect()->route('jobs.index')
+            ->with('success', 'Job created successfully.');
     }
 
     /**
@@ -46,7 +55,8 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
-        //
+        $job = Job::find($job->id);
+        return view('jobs.show', compact('job'));
     }
 
     /**
@@ -54,7 +64,8 @@ class JobController extends Controller
      */
     public function edit(Job $job)
     {
-        //
+        $job = Job::find($job->id);
+        return view('jobs.edit', compact('job'));
     }
 
     /**
@@ -62,7 +73,16 @@ class JobController extends Controller
      */
     public function update(UpdateJobRequest $request, Job $job)
     {
-        //
+        // Add logic for the update method, fields are title, description, salary, location
+        $job = Job::find($job->id);
+        $job->title = $request->title;
+        $job->description = $request->description;
+        $job->salary = $request->salary;
+        $job->location = $request->location;
+        $job->save();
+
+        return redirect()->route('jobs.index')
+            ->with('success', 'Job updated successfully');
     }
 
     /**
@@ -70,6 +90,11 @@ class JobController extends Controller
      */
     public function destroy(Job $job)
     {
-        //
+        // Add logic for the destroy method
+        $job = Job::find($job->id);
+        $job->delete();
+
+        return redirect()->route('jobs.index')
+            ->with('success', 'Job deleted successfully');
     }
 }
